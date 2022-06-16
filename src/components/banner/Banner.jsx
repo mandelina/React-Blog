3d11afaead02d2c983b0e25017eda85d06a3866d
minju moon <mmj9908@gmail.com>
2022-06-16 22:12:49 +0900

    banner

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Baaner from "./banner.css";
+
+export default function Banner() {
+  return (
+    <div class="banner">
+      <div class="max-width">
+        <div class="banner-contents">
+          <p class="sub-text">React Blog</p>
+          <p class="main-text">My BLOG</p>
+          <p class="description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+            nostrum dolor provident beatae deserunt magnam, illo neque. Maiores
+            ipsum commodi dolorum beatae aliquid laboriosam est
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
