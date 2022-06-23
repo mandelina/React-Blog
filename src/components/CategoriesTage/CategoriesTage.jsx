38b55942dad4cc41881bb1c5e2c2f2b4b653ca48
minju moon <mmj9908@gmail.com>
2022-06-23 23:08:46 +0900

    Categories tage들 구현

---
+++ src/components/CategoriesTage/CategoriesTage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./categoriesTage.css";
+
+export default function CategoriesTage() {
+  return (
+    <ul className="categories">
+      <li>
+        <a href="#">Life</a>
+      </li>
+      <li>
+        <a href="#">Style</a>
+      </li>
+      <li>
+        <a href="#">Tech</a>
+      </li>
+      <li>
+        <a href="#">Sport</a>
+      </li>
+      <li>
+        <a href="#">Photo</a>
+      </li>
+      <li>
+        <a href="#">Develop</a>
+      </li>
+      <li>
+        <a href="#">Music</a>
+      </li>
+    </ul>
+  );
+}
